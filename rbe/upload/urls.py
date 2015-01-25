76011e2167fea9146930d61497d3dbdb6dfd38c1
Vector919 <manbitesdog326@gmail.com>
2015-01-25 03:30:39 -0500

    fix for post on upload view

--- rbe/upload/urls.py
+++ rbe/upload/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-	url(r'^$', views.UploadView.as_view()),
+	url(r'^$', csrf_exempt(views.UploadView.as_view())),
 ]
