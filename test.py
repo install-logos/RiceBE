9a3e6617dc8f96bfb0f5a7dc7c3e590fcfa2dc79
Luminarys <kizunanohikari@gmail.com>
2015-04-15 22:00:16 +0200

    Outlined tests

---
+++ test.py
@@ -0,0 +1,10 @@
+#Backend tests to be implemented
+
+#Test 1 - Succesful setup/running of server
+#Test 2 - Succesful retrieval of query
+#Test 3 - Correct query return
+
+#Test 4 - Succesful upload of POST request
+#Test 5 - Succesful retrieval of newly inserted Rice
+#Test 6 - Succesful filtering of spam requests
+
