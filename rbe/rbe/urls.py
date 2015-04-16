68a630e5976d654112106b0d948c21e32065266a
Luminarys <kizunanohikari@gmail.com>
2015-04-16 04:36:05 +0200

    Added requirements

--- rbe/rbe/urls.py
+++ rbe/rbe/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'rbe.views.home', name='home'),
+    url(r'^$', 'rbe.views.index', name='home'),
     # url(r'^blog/', include('blog.urls')),
     
     url(r'^admin/', include(admin.site.urls)),
