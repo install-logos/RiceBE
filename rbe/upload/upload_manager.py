586bc1025babddbf6953b2675f3142c7f4be2b21
Luminarys <kizunanohikari@gmail.com>
2015-04-19 23:06:44 +0200

    Fixed rice downloading

--- rbe/upload/upload_manager.py
+++ rbe/upload/upload_manager.py
@@ -1,5 +1,5 @@
 """Handle Uploads from client"""
-import os, urllib.request, json, zipfile
+import os, urllib2, json, zipfile
 from rice_verify import checkSpam, checkPlagarism
 from query.models import Package
 
@@ -16,19 +16,37 @@ class UploadManager(object):
 	def upload(self):
 		upload_response = {}
 		temp = PACKAGE_DIR + "temp.zip"
-		urllib.request.urlretrieve(upstream, temp)
-		if not (os.path.exists(temp) or not zipfile.is_zipfile(temp_file)):
+		uopener = urllib2.urlopen(self.upstream_url)
+		data = uopener.read()
+		with open(temp,"wb") as fout:
+			fout.write(data)
+		if not (os.path.exists(temp) and zipfile.is_zipfile(temp)):
 			upload_response['error'] = "BAD URL or Zipfile"
 			return upload_response
+		print("Downloaded file")
 
 		z = zipfile.ZipFile(temp)
-		if INFO_FILE not in z.namelist():
-			upload_response['error'] = 'Missing package metadata'
-			return upload_response
 
+		print("Unzipping file")
  		for name in z.namelist():
 			z.extract(name, PACKAGE_DIR + "temp/")
 
+		os.chdir(PACKAGE_DIR + "temp")
+		path_files = os.listdir('.')
+ 		if len(path_files) == 1 and os.path.isdir(path_files[0]):
+            		os.chdir(path_files[0])
+            		for f in os.listdir('.'):
+                		os.rename('./'+f,'../'+f)
+        		os.chdir('../')
+       			os.rmdir(path_files[0])
+	
+		
+		
+		if INFO_FILE not in os.listdir('.'):
+			print(os.listdir('.'))
+			print('info file missing')
+			upload_response['error'] = 'Missing package metadata'
+			return upload_response
 		# if checkSpam():
 		#	upload_response['error'] = 'Package is SPAM'
 		#	return upload_response
@@ -40,12 +58,15 @@ class UploadManager(object):
 		# create and save the package
 		with open(INFO_FILE) as f:
 			upload_data = json.loads("".join(f.readlines()))
-
+		print(json.dumps(upload_data))
 		# this could fail if they pass some bad data
 		# TODO: figure out how we want this to behave in that case
 		uploaded_package = Package(**upload_data)
 		uploaded_package.save()
-
 		uploaded_package['success'] = "Package uploaded successfully!"
+		
+		# Clean up files
+		for name in z.namelist():
+			os.remove(PACKAGE_DIR + "temp/" + name)
 
 		return upload_response
